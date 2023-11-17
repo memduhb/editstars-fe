d7c31559d002e074ab61a56e997ff8a252093be9
memduh <memduhbayram0@gmail.com>
2023-11-17 22:09:08 +0300

    first commit

---
+++ types/index.d.ts
@@ -0,0 +1,17 @@
+declare module "*.jpg" {
+    const path: string;
+    export default path;
+}
+
+declare module "*.png" {
+    const path: string;
+    export default path;
+}declare module "*.jpg" {
+    const path: string;
+    export default path;
+}
+
+declare module "*.png" {
+    const path: string;
+    export default path;
+}
